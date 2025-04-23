c07d52c1badcfd241ad6100aa5d74dff397f6d95
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-04-23 11:43:59 -0400

    (FG): added new basic launch file for Bender

---
+++ launch/robot/bender_basic.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rosaria2',
+            executable='rosaria2',
+            name='rosaria2',
+            output='screen',
+        ),
+        Node(
+            package='urg_node2',
+            executable='urg_node2',
+            name='urg_node2',
+            output='screen',
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0.15', '0', '0.2', '0', '0', '0', 'base_link', 'laser'],
+            output='screen',
+        ),
+    ])
