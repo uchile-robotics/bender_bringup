bb4a159bd701924549026b79fffc93bb6262bbfe
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-05-18 09:26:10 -0400

    (FG): added hokuyo and pioneer params

--- launch/robot/bender_basic.launch.py
+++ launch/robot/bender_basic.launch.py
@@ -1,56 +1,48 @@
 from launch import LaunchDescription
-from launch_ros.actions import LifecycleNode, Node
-from launch.actions import ExecuteProcess, TimerAction
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+
 import os
 
 def generate_launch_description():
-    config_path = os.path.join(
-        get_package_share_directory('urg_node2'),
-        'config',
-        'params_serial.yaml'
+    urg_node_dir = FindPackageShare('urg_node2')
+    bringup_pkg = FindPackageShare('bender_bringup')
+
+    base_params = PathJoinSubstitution([
+        bringup_pkg,
+        'params',
+        'robot',
+        'pioneer.yaml'
+    ])
+    urg_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                urg_node_dir,
+                'launch',
+                'urg_node2.launch.py'
+            ])
+        )
     )
+
+    
+
     rosaria2_node = Node(
         package='rosaria2',
         executable='rosaria2_node',
         name='rosaria2',
         output='screen',
+        parameters=[base_params]
     )
 
-    laser_tf = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        arguments=['0.15', '0', '0.2', '0', '0', '0', 'base_link', 'laser'],
-        output='screen',
-    )
-    # Nodo lifecycle
-    urg_node = LifecycleNode(
-        package='urg_node2',
-        executable='urg_node2_node',
-        name='urg_node2',
-        namespace='',  # Se añade el namespace vacío
-        output='screen',
-        parameters=[config_path]
-    )
-
-    # Comando para configurar el nodo
-    configure_cmd = ExecuteProcess(
-        cmd=['ros2', 'lifecycle', 'set', '/urg_node2', 'configure'],
-        output='log'
-    )
-
-    # Comando para activar el nodo
-    activate_cmd = ExecuteProcess(
-        cmd=['ros2', 'lifecycle', 'set', '/urg_node2', 'activate'],
-        output='log'
-    )
+    
+    
 
     return LaunchDescription([
         rosaria2_node,
-        laser_tf,
         urg_node,
-        TimerAction(period=0.5, actions=[configure_cmd]),
-        TimerAction(period=1.0, actions=[activate_cmd]),
     ])
 
 
